b0cb9293baca868b35f37cb1d71b0f2feb232e1f
Golam Azam <bcdazam@gmail.com>
2024-05-25 16:32:06 +0600

    arguments practice

--- prac/pr.js
+++ prac/pr.js
@@ -26,19 +26,43 @@
 
 
 
-for(let i = 0; i < n.length; i++){
-    let element = n[i];
-    console.log(element);
+// for(let i = 0; i < n.length; i++){
+//     let element = n[i];
+//     console.log(element);
+// }
+
+// function doubleOnlyEven(n){
+//     if(element % 2 == 0){
+//         return element;
+//     }
+//     else{
+//         return element;
+//     }
+// }
+
+// let numberOne = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+// doubleOnlyEven(numberOne)
+
+// function evenify(num){
+//     if(num % 2 == 0){
+//         return num;
+//     }
+//     else{
+//         return num * 2;
+//     }
+// }
+
+// let result = evenify(13)
+// console.log('Result',result);
+
+function addNumbers(){
+    let sum = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        const element = arguments[i];
+        sum = sum + element;
+    }
+    return sum;
 }
 
-function doubleOnlyEven(n){
-    if(element % 2 == 0){
-        return element;
-    }
-    else{
-        return element;
-    }
-}
-
-let numberOne = [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-doubleOnlyEven(numberOne)
+let result = addNumbers(3, 5, 2, 5, 25);
+console.log(result);
