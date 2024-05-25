20f9bab1a63763117de767fdb7c35c30dcb404a7
Golam Azam <bcdazam@gmail.com>
2024-05-25 10:51:16 +0600

    even and odd number practice

--- prac/pr.js
+++ prac/pr.js
@@ -1,6 +1,19 @@
-let arr = [10, 20, 30, 40, 50];
+// let arra = [10, 20, 30];
 
-for(let i = 0; i < arr.length; i++){
-    let element = arr[i];
-    console.log('Double it: ', element, 'x 2 =', element * 2);
+// for(let i = 0; i < arra.length; i++){
+//     let sum = arra[i];
+//     console.log(sum * 2);
+// }
+
+let evenAddNumber = [10, 11, 23, 24, 12, 17, 18, 90];
+
+for(let i = 0; i < evenAddNumber.length; i++){
+    let element = evenAddNumber[i];
+    // console.log(element);
+    if(element % 2 == 0){
+        console.log(element, 'is an even number');
+    }
+    else{
+        console.log(element, 'is odd number');
+    }
 }
